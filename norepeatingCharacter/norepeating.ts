032f17d112c24d25ac988c6c4da30c94bcf26c8e
willianwt2000 <willianmetelien@gmail.com>
2025-02-13 23:58:36 -0400

    validateparentesis incomplete

---
+++ norepeatingCharacter/norepeating.ts
@@ -0,0 +1,19 @@
+function isValid(s: string): boolean {
+
+  let arrayString: string[] = s.split("");
+
+
+
+
+  for (let i = 0; i < arrayString.length; i++) {
+
+    if ((arrayString[i] === "(" && arrayString[i + 1] === ")") || (arrayString[i] === "[" && arrayString[i+1] === "]"))
+    {
+      return true;
+    }
+    
+  }
+
+  return false;
+    
+};
