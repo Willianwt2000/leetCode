3266dcd004de0f44872fa2bdcccfeb8465323846
willianwt2000 <willianmetelien@gmail.com>
2025-02-16 23:24:46 -0400

    Start roman number

--- norepeatingCharacter/noRepeting.js
+++ norepeatingCharacter/noRepeting.js
@@ -5,18 +5,16 @@ function isValid(s) {
         "[": "]",
         "{": "}",
     };
-    //1  2  3  4
-    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) { //clave o index       (  [  ]  )
+    for (var _i = 0, s_1 = s; _i < s_1.length; _i++) {
         var char = s_1[_i];
-        if (characters[char]) { //valor   )  ]
-            stack.push(char); // stack = [,       ]
+        if (characters[char]) {
+            stack.push(char);
         }
         else {
-            var end = stack.pop(); // end = [, end = ( ,     ] !== ] = false -*------ ) !== ) = false
-            //   console.log(end)
+            var end = stack.pop();
             if (characters[end] !== char) {
-                console.log(char); // 
-                return false; //false          
+                console.log(char);
+                return false;
             }
         }
     }
