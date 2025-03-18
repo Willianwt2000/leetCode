4084e7a6b92f0b0306fbb7f1858cb479fc70d100
willianwt2000 <willianmetelien@gmail.com>
2025-03-18 17:12:43 -0400

    Removing duplicate

--- 26removeDuplicates/index.ts
+++ 26removeDuplicates/index.ts
@@ -1,8 +1,26 @@
-function removeDuplicates(nums: number[]): number {
-    
+/*
+  Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in
+ */
 
-  return 0;
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+
+    let uniquePoint = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+
+      if (nums[i] !== nums[uniquePoint]) {
+        uniquePoint++;
+        nums[uniquePoint] = nums[i];
+
+      
+      }
+      
+    }
+
+    return uniquePoint + 1;
 };
 
 
 
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
