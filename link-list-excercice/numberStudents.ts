84f3f9ca2ea5de7e01a70e6560654d42ea7e946b
willianwt2000 <willianmetelien@gmail.com>
2025-04-06 11:37:00 -0400

    soluction Done!

--- link-list-excercice/numberStudents.ts
+++ link-list-excercice/numberStudents.ts
@@ -1,31 +1,51 @@
 function countStudents(students: number[], sandwiches: number[]): number {
-
   //crear un contador para almacenar los estudiante que se quedaron sin comer
   let studentWhitoutEating: number = 0;
+  let removeStudent; //number | undefined
 
-  //Estudiantes
-  for (let i = 0; i < sandwiches.length; i++) {
-      //sándwiches 
-      for (let j = 0; j < students.length; j++) {
-          //si el estudiante tomo su almuerzo querido -- sale de la fila
-          if((students[i] === 1) && (sandwiches[j] ===1) || (students[i] === 0) && (sandwiches[j] ===0)) {
-              students.splice(i, 1);
-              sandwiches.splice(i, 1);
+  // //Estudiantes
+  // for (let i = 0; i < sandwiches.length; i++) {
+  //   //sándwiches
+  //   for (let j = 0; j < students.length; j++) {
+  //     console.log("estudiantes "+students)
+  //     console.log("sandwiches "+sandwiches)
+  //     if (students[0] === sandwiches[0]) { //[1], [0]
+  //       students.shift();
+  //       sandwiches.shift();
+  //       studentWhitoutEating = 0;
+  //       console.log("----------------------------------\n")
+  //       console.log("estudiantes "+students)
+  //       console.log("sandwiches "+sandwiches)
+  //     } else if(sandwiches[0] !== students[0]) {  // [  1,0], [0, 1]
+  //       removeStudent = students.shift();
+  //       students.push(removeStudent);
+  //       studentWhitoutEating++;
+  //     }
 
-              studentWhitoutEating++;
-          } else  { 
-          //si no - se posiciona al final y vuelve hacer el proceso de elección.
-          let studentRemove: number = students.splice(0,1)[0]; //que debo de agregar en esta linea?
-          
-              console.log(studentRemove)
-              students.push(studentRemove);
-          }
+  //   }
 
-      }
+  // }
+
+  while (students.length > 0 && studentWhitoutEating < students.length) {
+    if (students[0] === sandwiches[0]) {
+      students.shift();
+      sandwiches.shift();
+      studentWhitoutEating = 0;
+    } else {
+      removeStudent = students.shift();
+      students.push(removeStudent);
+      studentWhitoutEating++;
+    }
   }
 
-  return studentWhitoutEating;
-
-};
-
-console.log(countStudents([1,1,0,0],[0,1,0,1]))
+  return students.length;
+}
+console.log(
+  countStudents(
+    [1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0]
+  )
+);
+console.log(countStudents([1,1,1,0,0,1],[1,0,0,0,1,1])); //3
+console.log(countStudents([1,0,1,0], [1,1,0,0])); //0
+console.log(countStudents([0,1,1,0], [0,1,0,1])); //0
