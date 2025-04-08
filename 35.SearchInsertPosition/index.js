28d5213e50b629cbce5d32e6b751c9b0d2703752
willianwt2000 <willianmetelien@gmail.com>
2025-04-08 02:31:34 -0400

    leetcode 35 search position

---
+++ 35.SearchInsertPosition/index.js
@@ -0,0 +1,15 @@
+function searchInsert(nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+        else if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return nums.length;
+}
+;
+console.log(searchInsert([1, 3, 5, 6], 5)); //2
+console.log(searchInsert([1, 3, 5, 6], 2)); //1
+console.log(searchInsert([1, 3, 5, 6], 7)); //4
