b9301ba59ce8a2b7723e4cd2805518eef4d0d8b0
willianwt2000 <willianmetelien@gmail.com>
2025-04-16 17:26:02 -0400

    solve

--- 21.MergeTwoSortedLists/index.ts
+++ 21.MergeTwoSortedLists/index.ts
@@ -1,22 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
 
-  if(list1 === null || list2 === null) return null;
-  let current : ListNode = list1;
-  let current2 : ListNode = list2;
-
-   while(current.next !== null && current2.next !== null) {
-       if(current.val <= current2.val) {
-           console.log(current.val);
-           console.log(current2.val);
-
-           current2.next = current; //target
-           current2 = current2.next;
-       } 
-       current = current.next;
-   }
+    if (!list1 && !list2) return null;
+    
+    let current1: ListNode | null = list1;
+    let current2: ListNode | null = list2;
+    let head: ListNode | null = new ListNode();
+    let current3 = head;
 
 
-return list2;
+
+
+    while (current1 !== null && current2 !== null) {
+        if (current1.val <= current2.val) {
+            current3.next = current1
+            current1 = current1.next;
+        } else {
+            current3.next = current2;
+            current2 = current2.next
+        }
+        current3 = current3.next;
+    }
+
+    console.log("List 3")
+    console.log(head)
+
+    if (current1 !== null) {
+        current3.next = current1
+    } else {
+        current3.next = current2
+    }
+
+
+
+    return head.next;
 
 
 };
