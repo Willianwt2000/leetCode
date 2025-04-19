c36683c5c6246de28ad77aed49c28ab5ba3a9e98
willianwt2000 <willianmetelien@gmail.com>
2025-04-18 20:37:22 -0400

    single number

---
+++ SingleNumber/index.ts
@@ -0,0 +1,17 @@
+function singleNumber(nums: number[]): number {
+
+  let uniqueNumber: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    uniqueNumber ^= nums[i]
+    
+  }
+
+  
+  return uniqueNumber
+};
+
+
+console.log(singleNumber([2,2,1])); // 1
+console.log(singleNumber([4,1,2,1,2])); // 4
+console.log(singleNumber([4,1,2,1,2])); // 4
