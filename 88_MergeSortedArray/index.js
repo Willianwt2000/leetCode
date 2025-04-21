3851a89ad0ff5dc10640cb569ae63c1fed22ff67
willianwt2000 <willianmetelien@gmail.com>
2025-04-20 23:08:07 -0400

    incompleto

---
+++ 88_MergeSortedArray/index.js
@@ -0,0 +1,6 @@
+function merge(nums1, m, nums2, n) {
+}
+;
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); //[1,2,2,3,5,6]
+console.log(merge([1], 1, [], 0)); //[1]
+console.log(merge([0], 0, [1], 1)); //[1]
