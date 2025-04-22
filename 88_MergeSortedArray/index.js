9441953791d5277789f140e514563f962dc0d952
willianwt2000 <willianmetelien@gmail.com>
2025-04-21 23:39:14 -0400

    complete

--- 88_MergeSortedArray/index.js
+++ 88_MergeSortedArray/index.js
@@ -1,21 +1,11 @@
 function merge(nums1, m, nums2, n) {
-    var size = m + n;
-    var k = 0;
-    console.log(nums1[k]);
-    if (nums1.length <= 1) {
-        nums1[k] = nums2[k];
-        nums1.shift();
-    }
-    else {
-        nums1.splice(-3);
-    }
+    //let size = m + n;
     for (var i = 0; i < n; i++) {
-        nums1.push(nums2[i]);
-        k++;
+        nums1[m + i] = nums2[i];
     }
     nums1.sort(function (a, b) { return a - b; });
     console.log(nums1);
-    return nums1;
+    return;
 }
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); //[1,2,2,3,5,6]
 console.log(merge([1], 1, [], 0)); //[1]
--- 88_MergeSortedArray/index.ts
+++ 88_MergeSortedArray/index.ts
@@ -1,27 +1,17 @@
-function merge(nums1: number[], m: number, nums2: number[], n: number): number[] {
-  let size = m + n;
-  let k = 0;
-  console.log(nums1[k]);
 
-  if (nums1.length <= 1) {
-    nums1[k] = nums2[k];
-    nums1.shift();
-  } else {
-    nums1.splice(-3);
-  }
-  for (let i = 0; i < n; i++) {
-    nums1.push(nums2[i]);
-    k++;
-  }
-
-  nums1.sort((a, b) => a - b);
-  
-  console.log(nums1);
-  return nums1;
+function merge(nums1: number[], m: number, nums2: number[], n: number): void{
+    for (let i = 0; i < n; i++) {
+      nums1[m+i] = nums2[i]
+    }
+    nums1.sort((a,b) => a-b);
+    return 
 }
 
 
 
 console.log(merge([1,2,3,0,0,0],3,[2,5,6],3)); //[1,2,2,3,5,6]
+
+
+
 console.log(merge([1],1,[],0)); //[1]
 console.log(merge([0],0,[1],1)); //[1]
