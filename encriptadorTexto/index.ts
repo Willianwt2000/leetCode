b684050ee8f6090292ae12a95b087d9e9c3939db
willianwt2000 <willianmetelien@gmail.com>
2025-05-01 08:40:01 -0400

    text encripted

---
+++ encriptadorTexto/index.ts
@@ -0,0 +1,58 @@
+function enCrypt(text: string) : string | undefined{
+  let textArr: string[] = text.split("");
+  
+  for (let i = 0; i < text.length; i++) {
+    console.log(text[i])
+    if (text[i] === "a") {
+      textArr[i] = "ai";
+    } else if (text[i] === "e") {
+      textArr[i] = "enter";
+    }  else if (text[i] === "i") {
+      textArr[i] = "imes";
+    }  else if (text[i] === "o") {
+      textArr[i] = "ober";
+    }  else if (text[i] === "u") {
+      textArr[i] = "ufat";
+    }
+  }
+
+  return textArr.join('').toLowerCase();
+}
+
+console.log(enCrypt("Hola como estas"));
+
+
+
+
+
+
+function deCrypt(text:string) : string | undefined {
+  let resultado = "";
+  let i = 0;
+
+  while (i < text.length) {
+    if (text.startsWith("ai", i)) {
+      resultado += "a";
+      i += 2; 
+    } else if (text.startsWith("et", i)) {
+      resultado += "e";
+      i += 2;
+    } else if (text.startsWith("imes", i)) {
+      resultado += "i";
+      i += 4; 
+    } else if (text.startsWith("ober", i)) {
+      resultado += "o";
+      i += 4; 
+    } else if (text.startsWith("ufat", i)) {
+      resultado += "u";
+      i += 4;
+    } else {
+      resultado += text[i];
+      i++;
+    }
+  }
+
+  return resultado.toLowerCase();
+}
+
+console.log(deCrypt("hoberlai cobermober enterstais"))
