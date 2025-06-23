5b1aae53c79de593d2b182587255128f3afb1888
willianwt2000 <willianmetelien@gmail.com>
2025-06-22 23:11:36 -0400

    ✅  closure

---
+++ 1957Delete/index.ts
@@ -0,0 +1,22 @@
+function makeFancyString(s: string): string {
+    
+
+  return ""
+};
+
+
+console.log(makeFancyString("leeetcode"))
+console.log(makeFancyString("aabaaaa"))
+
+
+
+// function outer() {
+//     let outerVar = `Si un código funciona en tu máquina pero no en producción… 
+//     ¿existió realmente?`;
+//     function inner() {
+//         console.log(outerVar);
+//     }
+//     return inner;
+// }
+// const closure = outer(); 
+// closure();
