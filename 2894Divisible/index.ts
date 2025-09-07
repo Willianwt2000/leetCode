3e4cdac65eef583a28243b0024bdd01f17203d8a
willianwt2000 <willianmetelien@gmail.com>
2025-09-07 14:04:16 -0400

    2894 Divisible and Non-divisible sum Difference

---
+++ 2894Divisible/index.ts
@@ -0,0 +1,14 @@
+function differenceOfSums(n: number, m: number): number {
+    let sum1: number = 0;
+    let sum2: number = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % m != 0) {
+            sum1 += i;
+        } else {
+            sum2 += i;
+        }
+
+    }
+
+    return sum1 - sum2;
+};
