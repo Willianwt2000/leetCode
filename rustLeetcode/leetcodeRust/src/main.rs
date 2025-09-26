8a60024530fb280d668c2a7d20b018c8d83c23f2
willianwt2000 <willianmetelien@gmail.com>
2025-09-26 11:20:35 -0400

    ADD Digit2

--- rustLeetcode/leetcodeRust/src/main.rs
+++ rustLeetcode/leetcodeRust/src/main.rs
@@ -1,28 +1,13 @@
 pub fn add_digits(num: i32) -> i32 {
-
-    let str_nums = num.to_string();
-    let mut total_sum = 0;
-
-    for  i in 0..str_nums.len()  {
-        let current_char = str_nums.chars().nth(i).unwrap(); 
-        let current_char2 = str_nums.chars().nth(i + 1).unwrap(); 
-
-        let digit_result: Option<u32> = current_char.to_digit(10);
-        let digit_result2: Option<u32> = current_char2.to_digit(10);
-
-
-        println!("{}",i);
-        if total_sum >= 0 {
-            total_sum = digit_result + digit_result2 
-        }
+    if num == 0 {
+        return 0;
     }
 
-
-
-    0
+    println!("Debug: {}",1 +(num - 1) % 9);
+    return 1 + (num - 1) % 9;
 }
 
 fn main() {
-    let num = 38;
+    let num = 3;
     println!("{}", add_digits(num));
 }
